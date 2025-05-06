d8606db593750f95e0ae96c5b451d990ba700812
Pallavaram Sundar Lal <2387683@cognizant.com>
2025-05-06 10:36:38 +0530

    home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // Default route (Home Page)
+  { path: '**', redirectTo: '', pathMatch: 'full' } // Redirect unknown routes to home
+];
